d46490e91d4779efab0e0d26d3351fde80732a82
MaggieKuo <perfumekuo@gmail.com>
2016-05-04 21:54:57 +0800

    icon gridView

--- app/src/main/java/com/etone/atm/MainActivity.java
+++ app/src/main/java/com/etone/atm/MainActivity.java
@@ -6,22 +6,41 @@ import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
+import android.widget.BaseAdapter;
+import android.widget.GridView;
+import android.widget.ImageView;
 import android.widget.ListView;
 import android.widget.Spinner;
+import android.widget.TextView;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
-import butterknife.OnItemSelected;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements AdapterView.OnItemSelectedListener, AdapterView.OnItemClickListener {
+    private static final int FUNC_BALANCE = 0;
+    private static final int FUNC_HISTORY = 1;
+    private static final int FUNC_NEWS = 2;
+    private static final int FUNC_FINANCE = 3;
+    private static final int FUNC_EXIT = 4;
+
     private final String TAG = this.getClass().getName();
     private static final int FUNC_LOGIN = 6;
 
     @BindView(R.id.list) ListView list;
     @BindView(R.id.spinner) Spinner spinner;
+    @BindView(R.id.gridView) GridView grid;
 
     private static boolean logon = false;
+    String[] func = {"餘額查詢", "交易明細", "最新消息", "投資理財", "離開"};
+    int icons[] = {R.drawable.func_balance,
+                    R.drawable.func_history,
+                    R.drawable.func_news,
+                    R.drawable.func_finance,
+                    R.drawable.func_exit};
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -31,22 +50,26 @@ public class MainActivity extends AppCompatActivity {
 
         if (!logon){
             startActivityForResult(new Intent(this, LoginActivity.class), FUNC_LOGIN);
-
         }
-        String[] func = {"餘額查詢", "交易明細", "最新消息", "投資理財", "離開"};
+
+        // ListView
         ArrayAdapter adapter = new ArrayAdapter(this, android.R.layout.simple_list_item_1, func);
         list.setAdapter(adapter);
+        list.setOnItemClickListener(this);
 
-/*
-        String[] data = getResources().getStringArray(R.array.notify_array);
-        ArrayAdapter adapter2 = new ArrayAdapter(this, android.R.layout.simple_list_item_1, data);
-*/
+        // spinner
+        /*
+                    String[] data = getResources().getStringArray(R.array.notify_array);
+                    ArrayAdapter adapter2 = new ArrayAdapter(this, android.R.layout.simple_list_item_1, data);
+                */
         ArrayAdapter adapter2 = ArrayAdapter.createFromResource(this,R.array.notify_array, android.R.layout.simple_list_item_1);
         spinner.setAdapter(adapter2);
-    }
+        spinner.setOnItemSelectedListener(this);
 
-    @OnItemSelected(R.id.spinner) void selected(){
-
+        //gridview
+        IconAdapter iconAdapter = new IconAdapter();
+        grid.setAdapter(iconAdapter);
+        grid.setOnItemClickListener(this);
     }
 
 
@@ -86,6 +109,82 @@ public class MainActivity extends AppCompatActivity {
         }
         // default --> return false;
         return super.onOptionsItemSelected(item);
+    }
+
+
+    @Override
+    public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+        String[] data = getResources().getStringArray(R.array.notify_array);
+        Log.d("item selected", position + " / " + data[position]);
+    }
+
+    @Override
+    public void onNothingSelected(AdapterView<?> parent) {
+
+    }
+
+    @Override
+    public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+        switch (parent.getId()){
+            case R.id.list:
+
+                break;
+
+            case R.id.gridView:
+                Log.d("grid view", position + "/");
+                switch (position){
+                    case FUNC_BALANCE:
+                        break;
+                    case FUNC_HISTORY:
+                        break;
+                    case FUNC_NEWS:
+                        break;
+                    case FUNC_FINANCE:
+                        break;
+                    case FUNC_EXIT:
+                        finish();
+                        break;
+                }
+
+
+
+                break;
+        }
+    }
+
+    class IconAdapter extends BaseAdapter{
+
+        @Override
+        public int getCount() {
+            return func.length;
+        }
+
+        @Override
+        public Object getItem(int position) {
+            return func[position];
+        }
+
+        @Override
+        public long getItemId(int position) {
+            return position;
+        }
+
+        @Override
+        public View getView(int position, View convertView, ViewGroup parent) {
+            View view = convertView;
+            if (view == null){
+                view = getLayoutInflater().inflate(R.layout.icon,null);
+                TextView tv = (TextView) view.findViewById(R.id.icon_text);
+                ImageView iv = (ImageView) view.findViewById(R.id.icon_image);
+
+                tv.setText(func[position]);
+                iv.setImageResource(icons[position]);
+
+                convertView = view;
+            }
+
+            return convertView;
+        }
     }
 
 
